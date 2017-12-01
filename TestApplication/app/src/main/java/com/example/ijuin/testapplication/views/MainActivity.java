b0e2e7b27fab25eb39832f61c018a0ce77143fd9
Lê Võ Gia Khang <levogiakhang@gmail.com>
2017-12-01 21:55:37 +0700

    write,read a text to internal storage

--- TestApplication/app/src/main/java/com/example/ijuin/testapplication/views/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/ijuin/testapplication/views/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.crystal.crystalrangeseekbar.interfaces.OnRangeSeekbarChangeListener;
@@ -32,11 +33,17 @@ import com.example.ijuin.testapplication.viewmodels.ProfileViewModel;
 
 import com.example.ijuin.testapplication.viewmodels.SearchViewModel;
 
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 /**
  * Created by Khang Le on 11/21/2017.
@@ -240,14 +247,91 @@ public class MainActivity extends AppCompatActivity implements Observer<Object>
     public static class SettingFragment extends Fragment {
 
         public SettingFragment() {        }
+        TextView _txt;
+        Button _sound;
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState)
         {
             View view = inflater.inflate(R.layout.setting_fragment, container, false);
+            _txt = (TextView) view.findViewById(R.id.content);
+            _sound = (Button) view.findViewById(R.id.btn_change_sound);
 
+            _sound.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    writeToFile("Khang Dep Trai","settings", "users");
+                    _txt.setText(readFromFile("settings","users"));
+                }
+            });
             return view;
+        }
+
+
+
+        private void writeToFile(String content, String filePath, String fileName)
+        {
+            ContextWrapper cw = new ContextWrapper(getContext());
+
+            File directory = cw.getDir(filePath, Context.MODE_PRIVATE);
+
+            File mypath = new File(directory,fileName + ".txt");
+
+            FileOutputStream fos = null;
+            try
+            {
+                fos = new FileOutputStream(mypath);
+                fos.write(content.getBytes());
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                try
+                {
+                    fos.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        private String readFromFile(String filePath, String fileName) {
+
+            String content = "";
+            FileInputStream fis = null;
+            try
+            {
+                File f = new File(filePath, fileName+".txt");
+                fis = new FileInputStream(f);
+                int size = fis.available();
+                byte[] buffer = new byte[size];
+                fis.read(buffer);
+                content = new String(buffer);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                try
+                {
+                    fis.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return content;
         }
 
     }
